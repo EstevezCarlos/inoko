37533437cf756d844b5c2bec8dc82ad7f73cde61
Chuck Wheeler <archdoodle@gmail.com>
2023-01-18 13:54:55 +0100

    nothing

--- index.js
+++ index.js
@@ -6,33 +6,31 @@ const highlightjs	=	require('markdown-it-highlightjs')
 const fs			=	require('node:fs')
 
 
-{
-	const
+const
 
-		fun			=	err => { if (err) throw err }
-		,write		=	(out,str) => {
-			fs.mkdirSync(path.dirname(out), { recursive: true , flag:'w'});
-			fs.writeFile(out,str,fun)
-		}
-
-		,readMd		=	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
-		,md			=	markdownIt({breaks: true}).use(highlightjs, {inline:true})
-		,pugjs		= 	(file,obj) => pug.compileFile(file, {basedir:'./'})(obj)
-
-	const tools = {
-
-		md 			:	file => md.render(readMd(file))
-		,content 	:	(component,instance)		=>	md.render(readMd(`./models/content/${component}s/${instance}.md`))
-		,pug		:	(component,obj)				=> 	write(`./public/dist/${component}.html`,pugjs(`./views/${component}.pug`,obj))
-		,pugs		:	(component,instance,obj)	=>	write(`./public/dist/${component}s/${instance}.html`,pugjs(`./views/${component}.pug`,obj))
+	fun			=	err => { if (err) throw err }
+	,write		=	(out,str) => {
+		fs.mkdirSync(path.dirname(out), { recursive: true , flag:'w'});
+		fs.writeFile(out,str,fun)
 	}
 
-	const model = () => { return {
-		
-		...mttj.parseFileSync('./models/data.md')
-		,readMd		:	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
-		
-	}}
+	,readMd		=	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
+	,md			=	markdownIt({breaks: true}).use(highlightjs, {inline:true})
+	,pugjs		= 	(file,obj) => pug.compileFile(file, {basedir:'./'})(obj)
 
-	module.exports = tools,model
+const tools = {
+
+	md 			:	file => md.render(readMd(file))
+	,content 	:	(component,instance)		=>	md.render(readMd(`./models/content/${component}s/${instance}.md`))
+	,pug		:	(component,obj)				=> 	write(`./public/dist/${component}.html`,pugjs(`./views/${component}.pug`,obj))
+	,pugs		:	(component,instance,obj)	=>	write(`./public/dist/${component}s/${instance}.html`,pugjs(`./views/${component}.pug`,obj))
 }
+
+const model = () => { return {
+	
+	...mttj.parseFileSync('./models/data.md')
+	,readMd		:	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
+	
+}}
+
+module.exports = tools,model
