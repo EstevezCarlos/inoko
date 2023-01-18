00392f8055f7df32a4e5f8644835678751b13947
Chuck Wheeler <archdoodle@gmail.com>
2023-01-18 13:00:47 +0100

    decoupled module from cli

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 
-const mttj			=	require('mttj')
-const pug			=	require('pug')
-const path			=	require('path')
-const markdownIt	=	require('markdown-it')
-const highlightjs	=	require('markdown-it-highlightjs')
-const fs			=	require('node:fs')
+
 const cmd			=	require('./commands.js')
-const say			=	require('./say.js')
 
 const dirs			=	{
 	"controllers"	:	"controllers",
@@ -36,35 +30,3 @@ cmd.cmd( {dirs:dirs}, process.argv.slice(2) )
 
 
  
-// Module
-
-{
-	const
-
-		fun			=	err => { if (err) throw err }
-		,write		=	(out,str) => {
-			fs.mkdirSync(path.dirname(out), { recursive: true , flag:'w'});
-			fs.writeFile(out,str,fun)
-		}
-
-		,readMd		=	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
-		,md			=	markdownIt({breaks: true}).use(highlightjs, {inline:true})
-		,pugjs		= 	(file,obj) => pug.compileFile(file, {basedir:'./'})(obj)
-
-	const tools = {
-
-		md 			:	file => md.render(readMd(file))
-		,content 	:	(component,instance)		=>	md.render(readMd(`./models/content/${component}s/${instance}.md`))
-		,pug		:	(component,obj)				=> 	write(`./public/dist/${component}.html`,pugjs(`./views/${component}.pug`,obj))
-		,pugs		:	(component,instance,obj)	=>	write(`./public/dist/${component}s/${instance}.html`,pugjs(`./views/${component}.pug`,obj))
-	}
-
-	const model = () => { return {
-		
-		...mttj.parseFileSync('./models/data.md')
-		,readMd		:	path => fs.existsSync(path) ? fs.readFileSync(path).toString() : '???'
-		
-	}}
-
-	module.exports = tools,model
-}
