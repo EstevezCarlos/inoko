cd677097e7324e228a57311c1831a4a503d0a094
Chuck Wheeler <archdoodle@gmail.com>
2023-01-30 16:36:48 +0100

    pathing bugfix

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const path			=	require('path')
 const markdownIt	=	require('markdown-it')
 const highlightjs	=	require('markdown-it-highlightjs')
 const fs			=	require('node:fs')
-const df			=	require('data-forge')
+// const df			=	require('data-forge')
 
-const data = new df.DataFrame([
-	{ name: 'Alice', age: 25, city: 'New York' },
-	{ name: 'Bob', age: 30, city: 'Los Angeles' },
-	{ name: 'Charlie', age: 35, city: 'Chicago' },
-])
+// const data = new df.DataFrame([
+// 	{ name: 'Alice', age: 25, city: 'New York' },
+// 	{ name: 'Bob', age: 30, city: 'Los Angeles' },
+// 	{ name: 'Charlie', age: 35, city: 'Chicago' },
+// ])
 
-console.log(data.where(row => row.age > 25).toArray())
+// console.log(data.where(row => row.age > 25).toArray())
 const
 
 	fun			=	err => { if (err) throw err }
@@ -30,8 +30,8 @@ const
 module.exports  = {
 
 	content 	:	( beast,individual		)	=>	md.render(readMd(`./models/content/${beast}s/${individual}.md`))
-	,pugList	:	( beast,obj				)	=> 	write(`./public/dist/lists/${beast}.html`,pugjs(`./views/${beast}.pug`,obj))
-	,pugSingle	:	( beast,individual,obj	)	=>	write(`./public/dist/singles/${beast}s/${individual}.html`,pugjs(`./views/${beast}.pug`,obj))
+	,pugList	:	( beast,obj				)	=> 	write(`./public/dist/lists/${beast}.html`,pugjs(`./views/lists/${beast}.pug`,obj))
+	,pugSingle	:	( beast,individual,obj	)	=>	write(`./public/dist/singles/${beast}s/${individual}.html`,pugjs(`./views/singles/${beast}.pug`,obj))
 	,pugPage	:	( page,obj				)	=>	write(`./public/${page}.html`,pugjs(`./views/${page}.pug`,obj))
 	,model		:	( 						)	=>	mttj.parseFileSync('./models/data.md')
 
