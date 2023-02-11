cfebfbdfc76e60c66ad6422229096e213b88a152
Chuck Wheeler <archdoodle@gmail.com>
2023-02-11 09:52:20 +0100

    No dataframes - they suck

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const markdownIt	=	require('markdown-it')
 const highlightjs	=	require('markdown-it-highlightjs')
 const meta			=	require('markdown-it-meta')
 const fs			=	require('node:fs')
-const danfo			=	require('danfojs-node')
 
-// const data = new df.DataFrame([
-// 	{ name: 'Alice', age: 25, city: 'New York' },
-// 	{ name: 'Bob', age: 30, city: 'Los Angeles' },
-// 	{ name: 'Charlie', age: 35, city: 'Chicago' },
-// ])
-
-// console.log(data.where(row => row.age > 25).toArray())
 const
 
 	fun			=	err => { if (err) throw err }
@@ -35,19 +27,10 @@ module.exports  = {
 	content 	:	( beast,individual		)	=>	{
 		const path = `./models/content/${beast}s/${individual}.md`
 		const content = md.render(readMd(path))
-		// console.log(path,'-',content)
 		return content
 	}
 	,pugList	:	( beast,obj				)	=> 	write(	`./public/${beast}s.html`,					pugjs(`./views/beasts/${beast}/list.pug`,	obj)	)
 	,pugLone	:	( beast,individual,obj	)	=>	write(	`./public/${beast}s/${individual}.html`,	pugjs(`./views/beasts/${beast}/lone.pug`,	obj)	)
 	,pugPage	:	( page,obj				)	=>	write(	`./public/${page}.html`,					pugjs(`./views/pages/${page}.pug`,				obj)	)
-	,model		:	( 						)	=>	{
-		input = mttj.parseFileSync('./models/data.md')
-		output = {}
-		for (const [k,v] of Object.entries(input)) {
-			output[k] = new danfo.DataFrame(v)
-			console.log(k,output[k])
-		}
-		return output
-	}
+	,model		:	( 						)	=>	mttj.parseFileSync('./models/data.md')
 }
